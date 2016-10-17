d23d544be3cfc8b12b15c750427708482794f237
Steven Dash <scdash91@outlook.com>
2016-10-17 17:37:58 -0400

    Control Scheme Update
    Added friction and ball control modifications

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallController : MonoBehaviour {
+
+    public float force = 50;
+    public float speed;
+    public float turnSpeed;
+    public Rigidbody ball;
+    private bool space;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        force += Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        if (Input.GetKeyDown("space") && ball.velocity.magnitude < .5f) {
+            ball.AddForce(transform.forward * force, ForceMode.Impulse);
+            space = true;
+        }
+        if (space = true && ball.velocity.magnitude < .5f)
+        {
+            ball.velocity = new Vector3(0, 0, 0);
+            space = false;
+        }
+    }
+}
---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+
+    public Transform target;
+    public float speed;
+    public float distance;
+    private float currentAngle = 0;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        currentAngle += Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        Quaternion q = Quaternion.Euler(25, currentAngle, 0);
+        Vector3 direction = q * Vector3.forward;
+        transform.position = target.position - direction * distance;
+        transform.LookAt(target.position);
+    }
+}
